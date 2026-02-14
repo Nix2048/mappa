8e46bd506699995461bca17d5644bbde33da4cd7
Nix2048 <cn2p4cv8sy@privaterelay.appleid.com>
2026-02-14 17:16:07 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+  // Memaksa video agar langsung berputar secara inline
+  const video = document.getElementById('heroVideo');
+  video.muted = true; // Pastikan lagi via JS kalau ini di-mute
+  video.play();
